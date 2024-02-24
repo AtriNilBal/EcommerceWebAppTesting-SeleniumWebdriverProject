03fa7d9c41d060130d4e86fe19e1fe3e1c847845
AtriNilBal <atrinilbal.se@gmail.com>
2024-02-24 13:17:21 +0530

    added tests for edge and ff browser

---
+++ src/test/java/page/tests/UserLoginTestNgTestsOnFirefox.java
@@ -0,0 +1,42 @@
+package page.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import page.classes.CommonMethods;
+import page.classes.LandingPageFactory;
+import page.classes.LoginPageFactory;
+import page.constants.ExpectedValues;
+
+public class UserLoginTestNgTestsOnFirefox {
+
+    private WebDriver webdriver;
+    private LandingPageFactory landingPageFactory;
+    private LoginPageFactory loginPageFactory;
+
+    @BeforeClass
+    public void setup() {
+        //webdriver=new FirefoxDriver(CommonMethods.disableAutomatedTestsFlagInFirefox());
+        webdriver=new FirefoxDriver();
+        webdriver.get(ExpectedValues.BASE_URL);
+        webdriver.manage().window().maximize();
+        landingPageFactory=new LandingPageFactory(webdriver);
+        loginPageFactory=new LoginPageFactory(webdriver);
+    }
+
+    @Test
+    public void test() {
+        landingPageFactory.hoverSignInAccountsAndClickSignInButton();
+        loginPageFactory.userInputToUsernameTextBox(ExpectedValues.USER_LOGIN_USERNAME);
+        loginPageFactory.clickUserNameSubmitButton();
+        loginPageFactory.userInputToPasswordTextBox(ExpectedValues.USER_LOGIN_PASSWORD);
+        loginPageFactory.clickSignInButton();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        webdriver.quit();
+    }
+}
